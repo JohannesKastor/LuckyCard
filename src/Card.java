d5bde845b03a99b6acc0ae578b1229f31df2a6b9
JohannesWin <johanneshenningkastor@hotmail.com>
2025-01-24 13:05:16 +0100

    Har fixat Card, och Deck (classes, forsätta analysera Deck)

--- src/Card.java
+++ src/Card.java
@@ -1,20 +1,38 @@
-import java.util.Random;
-
 public class Card {
-
-
-	public static void (String[] args) {
-   
-    Random rand = new Random();
-    int CardNumber = rand.nextInt(14);
-
-
-
-    System.out.println(CardNumber);
-    
-    System.out.println(CardSymbol);
-    
-  }
-
-
+	private String Suit;
+	private int Number;
+	private int Value;
+	
+	public Card(String Suit, int Number) {
+		this.Suit = Suit;
+		this.Number = Number;
+		this.Value = Number + getSuitBonus(Suit);
+	}
+	
+	private int getSuitBonus(String Suit) {
+		switch (Suit) {
+		case "Diamonds": return 4;
+		case "Clubs": return 6;
+		case "Hearts": return 8;
+		case "Spades": return 10;
+		default: return 0;
+		
+		}
+				
+	}
+	
+	public int getValue() {
+		return Value;
+	}
+	
+	public int getNumber() {
+		return Number;
+	}
+	
+	public String getSuit() {
+		return Suit;
+	}
+	
+	
+	
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
-	Card CardInstance = new Card(10);
+	private List <Card> cards;
+	
+	public Deck() {
+		cards = new ArrayList<>();
+		String[] Suits = {"Diamonds", "Clubs", "Hearts","Spades"};
+		for (String suit: Suits) {
+			for (int rank =7; rank <= 12; rank++) {
+				cards.add(new Card(suit,rank));
+			}
+		}
+		
+		shuffle();
+		
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public Card draw() {
+		return cards.isEmpty() ? null : cards.remove(0);
+	}
+	
 }
